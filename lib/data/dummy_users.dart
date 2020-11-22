cff08deb3c7fa4e865ccce2abeb493cd9834a2b1
Diego Brito <dibbrito@gmail.com>
2020-11-22 12:44:01 -0300

    Finish crud

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:crud/models/user.dart';
+
+const DUMYY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png')
+};
